4268718131996e449be5a5af5a1151c88f9e61a8
Saulo Coelho <coelho.saulo@gmail.com>
2025-02-26 14:17:52 -0300

    solução desafio B e C

--- controle_fluxo/desafio_iteracao.py
+++ controle_fluxo/desafio_iteracao.py
@@ -5,6 +5,28 @@ numeros = [10, 25, 74, 62, 31, 2]
 soma = 0
 
 for numero in numeros:
-    soma = numero + soma
+    soma += numero
+
 print(f'A soma dos números é : {soma}')
 print(f'A média é: {soma/len(numeros)}')
+
+# Dado uma sequencia de números, calcule o maior valor da sequência.
+# ATENÇÂO: não vale usar a função max()
+
+numeros = [10, 24, 62, 87, 92]
+
+maximo = numeros[0]
+
+for numero in numeros:
+    if numero > maximo:
+        maximo = numero
+
+print(f'O maior valor é: {maximo}')
+
+# Dado uma  lista de palavras, print todas que tem pelo menos cinco caracteres
+
+palavras = ['casa', 'cachorro', 'gato', 'escola', 'telefone']
+
+for palavra in palavras:
+    if len(palavra) >= 5:
+        print(palavra)
