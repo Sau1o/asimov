c8620a338943309ca414bbf17b3a5047860df91f
Saulo Coelho <coelho.saulo@gmail.com>
2025-03-02 08:17:43 -0300

    update desafio02 restaurante.py

--- iteracao/desafio02_restaurante.py
+++ iteracao/desafio02_restaurante.py
@@ -23,10 +23,9 @@ bebidas = {
 
 combo = {}
 
-for chave_comida, preco_comida in comidas.items():
-    for chave_bebida, preco_bebida in bebidas.items():
-        chave_combo = (chave_comida, chave_bebida)
-        preco_combo = preco_comida + preco_bebida
-        combo[chave_combo] = round(preco_combo)
+for tupla in itertools.product(comidas.items(), bebidas.items()):
+    chave_combo = tuple(tup[0] for tup in tupla)
+    preco_combo = sum(tup[1] for tup in tupla)
+    combo[chave_combo] = round(preco_combo, 2)
 
 print(combo)
