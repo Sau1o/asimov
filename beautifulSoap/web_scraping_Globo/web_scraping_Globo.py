d0547981f727812c367c994eb3bbfe3badb171a3
Saulo Coelho <coelho.saulo@gmail.com>
2025-03-03 10:36:02 -0300

    update web scraping site de noticias Globo

---
+++ beautifulSoap/web_scraping_Globo/web_scraping_Globo.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.globo.com/'
+page = requests.get(url)
+
+resposta = page.text
+
+soup = BeautifulSoup(resposta, 'html.parser')
+
+noticias = soup.find_all('h2', {'class': 'post__title'})
+
+for i in range(len(noticias)):
+    print(noticias[i].text)
