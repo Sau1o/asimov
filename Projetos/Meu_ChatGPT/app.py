91d7754b3e42c1dfd271547a224dda5e3d05eb1b
Saulo Coelho <coelho.saulo@gmail.com>
2025-03-12 07:10:59 -0300

    finished project

--- Projetos/Meu_ChatGPT/app.py
+++ Projetos/Meu_ChatGPT/app.py
@@ -2,12 +2,65 @@ import streamlit as st
 import openai
 from dotenv import load_dotenv, find_dotenv
 
-import re
-from unidecode import unidecode
+from utils_files import *
 
 _ = load_dotenv(find_dotenv())
 
 client = openai.Client()
+
+# INICIALIZAÇÃO ==================================================
+
+
+def inicializacao():
+    if not 'mensagens' in st.session_state:
+        st.session_state.mensagens = []
+    if not 'conversa_atual' in st.session_state:
+        st.session_state.conversa_atual = ''
+    if not 'modelo' in st.session_state:
+        st.session_state.modelo = 'gpt-3.5-turbo'
+
+# TABS ==================================================
+
+
+def tab_conversas(tab):
+
+    tab.button('➕ Nova conversa',
+               on_click=seleciona_conversa,
+               args=('', ),
+               use_container_width=True)
+    tab.markdown('')
+    conversas = listar_conversas()
+    for nome_arquivo in conversas:
+        nome_mensagem = desconverte_nome_mensagem(nome_arquivo).capitalize()
+        if len(nome_mensagem) == 30:
+            nome_mensagem += '...'
+        tab.button(nome_mensagem,
+                   on_click=seleciona_conversa,
+                   args=(nome_arquivo, ),
+                   disabled=nome_arquivo == st.session_state['conversa_atual'],
+                   use_container_width=True)
+
+
+def seleciona_conversa(nome_arquivo):
+    if nome_arquivo == '':
+        st.session_state['mensagens'] = []
+    else:
+        mensagem = ler_mensagem_por_nome_arquivo(nome_arquivo)
+        st.session_state['mensagens'] = mensagem
+    st.session_state['conversa_atual'] = nome_arquivo
+
+
+def tab_configuracoes(tab):
+    modelo_escolhido = tab.selectbox('Selecione o modelo',
+                                     ['gpt-3.5-turbo', 'gpt-4'])
+    st.session_state['modelo'] = modelo_escolhido
+
+    # chave = tab.text_input('Adicione sua api key',
+    #                        value=st.session_state['api_key'])
+    # if chave != st.session_state['api_key']:
+    #     st.session_state['api_key'] = chave
+    #     salva_chave(chave)
+    #     tab.success('Chave salva com sucesso')
 
 
 def geracao_texto(mensagens):
@@ -26,24 +79,20 @@ def geracao_texto(mensagens):
             texto_completo += texto
     return texto_completo
 
-
-def salvar_mensagens(mensagens):
-    if len(mensagens) == 0:
-        return False
     nome_mensagem = ''
     for mensagem in mensagens:
         if mensagem['role'] == 'user':
-            nome_mensagem = mensagem['content'][30]
+            nome_mensagem = mensagem['content'][:30]
             break
-    nome_arquivo = unidecode(nome_mensagem)
-    nome_arquivo = re.sub('\W+', '', nome_arquivo).lower()
+    return nome_mensagem
 
 
+# PÁGINA PRINCIPAL ==================================================
 def pagina_principal():
     if not 'mensagens' in st.session_state:
         st.session_state.mensagens = []
 
-    mensagens = st.session_state['mensagens']
+    mensagens = ler_mensagens(st.session_state['mensagens'])
     st.header('🤖 Coelho ChatBot', divider=True)
 
     for mensagem in mensagens:
@@ -71,6 +120,17 @@ def pagina_principal():
         mensagens.append(nova_mensagem)
 
         st.session_state['mensagens'] = mensagens
+        salvar_mensagens(mensagens)
 
 
-pagina_principal()
+# MAIN ==================================================
+def main():
+    inicializacao()
+    pagina_principal()
+    tab1, tab2 = st.sidebar.tabs(['Conversas', 'Configurações'])
+    tab_conversas(tab1)
+    tab_configuracoes(tab2)
+
+
+if __name__ == '__main__':
+    main()
